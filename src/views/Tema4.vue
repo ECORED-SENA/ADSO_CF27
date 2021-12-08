912a4fc83dceacb99423255f9a4a1a1a677a07a1
John <83730668+DevJohn2021@users.noreply.github.com>
2021-12-07 19:02:37 -0500

    1er cargue tema4 numeracion

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -8,23 +8,23 @@
         span.text-white 4
       h1 Gestores de Contenido
     
-    #t_3_1.titulo-segundo.color-acento-contenido
+    #t_4_1.titulo-segundo.color-acento-contenido
       .h2 4.1 Definición 
     
     separador
-    #t_3_2.titulo-segundo.color-acento-contenido
+    #t_4_2.titulo-segundo.color-acento-contenido
       .h2 4.2 Funciones
 
     separador
-    #t_3_3.titulo-segundo.color-acento-contenido
+    #t_4_3.titulo-segundo.color-acento-contenido
       .h2 4.3 Características
 
     separador
-    #t_3_4.titulo-segundo.color-acento-contenido
+    #t_4_4.titulo-segundo.color-acento-contenido
       .h2 4.4 Clasificación CMS
 
     separador
-    #t_3_5.titulo-segundo.color-acento-contenido
+    #t_4_5.titulo-segundo.color-acento-contenido
       .h2 4.5 Tipos CMS
 
 
