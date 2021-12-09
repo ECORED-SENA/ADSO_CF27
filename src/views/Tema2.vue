6ba493ab0fe6f4fbb585cefc985b7e43c065a918
John <83730668+DevJohn2021@users.noreply.github.com>
2021-12-09 15:17:30 -0500

    avance 3

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -36,8 +36,8 @@
     p.mb-4 Al momento de implementar las CSS es necesario tener en cuenta los siguientes puntos:
 
 
-    .row.justify-content-center.mb-5
-      .col-10.col-sm-5.col-lg-5.mb-4.mb-lg-0
+    .row.justify-content-center(style="margin-bottom: 60px;")
+      .col-10.col-sm-10.col-lg-5.mb-4.mb-lg-0
         .tarjeta.p-5(data-aos="zoom-in" style=" background-color: #79E3D9;")
           .row.justify-content-center.mb-3
             .col-7
@@ -47,7 +47,7 @@
             b Separación de estructura del documento, 
             b-none se refiere a la separación del contenido del documento html independiente de su presentación (tipos de letras, colores, tamaños de letras, espacios, fondos de pantalla, anexo de imágenes, entre otros)
               
-      .col-10.col-sm-5.col-lg-5.mb-4.mb-lg-0
+      .col-10.col-sm-10.col-lg-5.mb-4.mb-lg-0
         .tarjeta.p-5(data-aos="zoom-in" style=" background-color: #4DC1FA;")
           .row.justify-content-center.mb-3
             .col-7
@@ -60,8 +60,59 @@
 
 
 
-    
-    
+    .row.justify-content-center(style="margin-bottom: 60px;")
+      .col-9.col-lg-4.pe-lg-0(style="background-color: #12263F")
+        .cajonp
+          p.mb-0.text-white
+            b Ejemplo de hoja interna:
+        .code.pt-1
+          code  &lt;head&gt; <br>&emsp;&emsp; &lt;style&gt; <br>&emsp;&emsp;&emsp; body{ background-color: #FFFFFF;  <br>&emsp;&emsp;&emsp; }  <br>&emsp;&emsp;&emsp; h1{  <br>&emsp;&emsp;&emsp;&emsp; color: #000000; <br>&emsp;&emsp;&emsp;&emsp; text-align: center; <br>&emsp;&emsp;&emsp; }  <br>&emsp;&emsp; &lt;/style&gt;  <br> &lt;/head&gt; <br> &lt;body&gt; <br> &lt;/body&gt;
+              
+      .col-9.col-lg-4.px-lg-0(style="background-color: #4DD0E1")
+        figure
+          img(src='@/assets/curso/tema2/img3.svg', alt='imagen ambiental', style="width: 400px;").m-auto
+          
+      .col-9.col-lg-4.ps-lg-0(style="background-color: #12263F")
+        .cajonp
+          p.mb-0.text-white
+            b Ejemplo de hoja interna:
+        .code.pb-0.pt-1
+          code  &lt;head&gt; <br>&emsp; &lt;link rel="stylesheet" type="text/css" href="styles.css"&gt;  <br> &lt;/head&gt; <br> &lt;body&gt; <br> &lt;/body&gt;
+        .cajonp.pb-0.pt-1
+          p.mb-0.text-white
+            b Ejemplo de hoja sitio <em>web</em>:
+        .code.pb-0.pt-1
+          code  &lt;head&gt; <br>&emsp; &lt;link rel="stylesheet" type="text/css" href="https://www.estilo.css"&gt;  <br> &lt;/head&gt; <br> &lt;body&gt; <br> &lt;/body&gt;
+          
+
+    .tarjeta.fondo-slider.p-4.mb-4
+      SlyderA(tipo="b")
+        .row.justify-content-center.align-items-center
+          .col-11.col-lg-5.mb-4.mb-md-0
+            p.mb-0 
+              b Selectores de CSS, 
+              b-none se utilizan para determinar los elementos a los que se va a aplicar el estilo y se clasifican por nombre, por id de selector y por clase.
+              
+          .col-10.col-lg-5
+            figure
+              img(src='@/assets/curso/tema2/img4.png', alt='imagen que acompaña', style="width: 500px;").m-auto
+          
+        .row.justify-content-center.align-items-center
+          .col-11.col-lg-5.mb-4.mb-md-0
+            p.mb-0 Ejemplo de selector por id en donde solo se aplica un elemento único qui el nombre del estilo va precedido por el carácter #.
+              
+          .col-10.col-lg-5
+            figure
+              img(src='@/assets/curso/tema2/img5.png', alt='imagen que acompaña', style="width: 500px;").m-auto
+          
+        .row.justify-content-center.align-items-center
+          .col-11.col-lg-5.mb-4.mb-md-0
+            p.mb-0 Ejemplo de selector de clase, en este caso se pueden personalizar los estilos para aplicarlos a diferentes elementos que comparten alguna relación para ello se debe escribir un punto(.) delante del nombre de la clase:
+              
+          .col-10.col-lg-5
+            figure
+              img(src='@/assets/curso/tema2/img6.png', alt='imagen que acompaña', style="width: 500px;").m-auto
+          
     
     
     separador
@@ -69,20 +120,103 @@
       .h2 2.2 Introducción CSS3
     
     
+
+    .row.fondo-gradiente1.mb-4
+      .col-lg-12.px-md-5.pt-0
+        .row.justify-content-center.align-items-center.mb-5
+          .col-lg-8.mb-4.mb-lg-0
+            p.mb-0
+              b-none.etiqueta CSS3 es la evolución de CSS y CSS2,
+              b-none  esta nueva versión presenta como novedad mejoras que a grandes rasgos pueden dividirse en dos características:
+              b-none.etiqueta la primera es la aplicación del concepto de módulos
+              b-none que permite aproximar la definición del estilo de un sitio web a partir de la programación; es decir, donde se puede hacer para ello la analogía a los patrones de diseño que implementan algún controlador, por lo que los módulos permiten definir contextos de aplicación de estilos e identificarlos en archivos diferentes (con extensión .less) que finalmente se integran en un archivo controlador
+              b-none.etiqueta La segunda es la posibilidad de enriquecer los elementos que conforman
+              b-none las páginas directamente desde el estilo con animaciones, transiciones, layout con columnas, gradientes para modificar colores, transformaciones para rotar elementos, así como también el ingreso de uso de fuente de texto externas por medio de librerías.
+            
+          .col-10.col-lg-4
+            figure
+              img(src='@/assets/curso/tema2/img7.svg', alt='imagen que acompaña', style="width: 420px;").m-auto
+          
+        .row.justify-content-center.mb-4
+          p.mb-0 Ejemplo de estructura de uso de módulos:
+        .row.justify-content-center.mb-5
+          .col-11.col-lg-8
+            figure.mb-lg-5
+              img(src='@/assets/curso/tema2/img8.svg', alt='imagen que acompaña', style="").m-auto
+
+
+
     
     
     separador
     #t_2_3.titulo-segundo.color-acento-contenido
       .h2 2.3 Estructura
     
+    .contenedor-total
+      figure
+        img.mx-auto(src='@/assets/curso/tema2/img9.jpg' alt='Imagen ambiental' style='')
+      .contenedor-text
+        .row.mb-4.mb-lg-0
+          .col-auto
+            p.mb-0 La estructura de CSS3 corresponde al conjunto de reglas recopiladas en un archivo; una regla a su vez corresponde a la utilización de algún tipo de selector como los que ya vimos en puntos anteriores y una o varias declaraciones separadas por punto y una (;), la declaración se conforma de una propiedad y un valor; dichas declaraciones se pueden escribir de manera directa en el tag de html deseado utilizando el atributo style y entre comillas la propiedad los dos puntos y su valor.
     
-    
-    
+    p.mb-4 Ejemplo de estructura: 
+
+    .row.fondo1.mb-5
+      .col-lg-12.px-md-5.py-5
+        .row.justify-content-center
+          .col-lg-11
+            figure
+              img.mx-auto(src='@/assets/curso/tema2/img10.svg' alt='Imagen ambiental' style='')
+
+    .row.justify-content-center.mb-4
+      .col-lg-3.mb-4.mb-lg-0
+        p.mb-0
+          b Con uso del atributo style:
+      .col-10.col-lg-9
+        .row
+          .col-lg-10
+            figure
+              img.mx-auto(src='@/assets/curso/tema2/img11.svg' alt='Imagen ambiental' style='')
+
+
+
     separador
     #t_2_4.titulo-segundo.color-acento-contenido
       .h2 2.4 Formularios CSS3
 
-    
+    .row.justify-content-center.align-items-center.mb-5
+      .col-10.col-lg-5
+        figure
+          img.mx-auto(src='@/assets/curso/tema2/img12.svg' alt='Imagen ambiental' style='width: 482px;')
+          
+      .col-lg-7
+        .cajon.color-acento-contenido.p-4
+          p.mb-0 Estos formularios se construyen de manera normal básicamente como lo vimos en los puntos anteriores, solo que en esta nueva versión se caracteriza por que se agregan nuevos tipos de datos (color, tel, email, date, url, time, month, number, week, datetime, range, search), nuevos controles (output) y nuevos atributos (placeholder, autofocus, min, required, max, step, pattern), para estos casos por recomendación de la W3c es necesario verificar su funcionamiento en los diferentes navegadores hasta el momento solo funcionan bien solo con el navegador Opera.
+
+
+    AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--gris")
+      .row(titulo="Ejemplo de formulario:")
+        .col-12
+          figure
+            img.mx-auto(src='@/assets/curso/tema2/img13.svg' alt='Imagen ambiental' style='')
+          
+
+    p.mb-4 El resultado de la ejecución del código anterior se puede visualizar a continuación:
+
+    .row.justify-content-center
+      .col-4.col-lg-3
+        a.boton.color-secundario.indicador__container(@click="modal1 = true")(style="border-radius: 100px; background: linear-gradient(to bottom, #039BE5, #027FBC);")
+          span.text-center 
+            b.text-white Ejecutar en el navegador
+          .indicador--click(v-if="mostrarIndicador")
+            
+    ModalA(:abrir-modal.sync="modal1")
+      .row
+        .col-12
+          figure.mb-4
+            img.m-auto(src='@/assets/curso/tema1/img16.png' alt='Imagen ambiental' style='')
+
 
 
 </template>
@@ -92,6 +226,8 @@ export default {
   name: 'Tema2',
   data: () => ({
     // variables de vue
+    mostrarIndicador: true,
+    modal1: false,
   }),
   mounted() {
     this.$nextTick(() => {
--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -5,15 +5,119 @@
     
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
-        span.text-white 3
+        span 3
       h1 JavaScript
     
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-7.mb-4.mb-lg-0
+        .p-5(style="background-color: #039BE5")
+          p.mb-0.text-white Es un lenguaje de programación que se inserta en una página <em>web</em> que es interpretado por los navegadores, el concepto de interpretado hace referencia a la ejecución secuencial línea por línea que hace el intérprete del navegador ante todo del lado cliente, aunque hoy en día también se logra del lado servidor con el apoyo Node.js el cual no es compilado como otros lenguajes de programación. Este lenguaje está orientado a brindar posibilidades de construcción de código html de manera dinámica; es decir, el programa que se crea construye el conjunto de tags html de manera automática multiplicando de manera sustancial el enriquecimiento del diseño de las interfaces <em>web</em>.	
+      .col-10.col-lg-5
+        figure
+          img(src='@/assets/curso/tema3/img1.svg', alt='imagen ambiental', style="width: 450px;").m-auto
+          
+
+
+
+
     #t_3_1.titulo-segundo.color-acento-contenido
       .h2 3.1 Introducción 
+
+    .contenedor-total
+      figure
+        img.mx-auto(src='@/assets/curso/tema3/img2.jpg' alt='Imagen ambiental' style='')
+      .contenedor-text
+        .row.mb-4.mb-lg-0
+          .col-lg-10
+            p.mb-0 Es importante tener en cuenta que JavaScript y Java con lenguajes diferentes, dado que por llevar el mismo nombre se puede presentar para confusiones; para insertar código JavaScript a una página web se puede hacer de las siguientes formas: 
+
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-9.col-lg-6.mb-4.mb-lg-0
+        figure
+          img.mx-auto(src='@/assets/curso/tema3/img3.svg' alt='Imagen ambiental' style='width:590px;')
+      .col-lg-6
+        ul.lista-ul.mb-0
+          li 
+            i.fas.fa-layer-group(style="color: #039BE5;")
+            p.mb-0 Con código que se agrega dentro de la misma página.
+              
+          li 
+            i.fas.fa-layer-group(style="color: #039BE5;")
+            p.mb-0 Con archivos independientes que se almacenan en rutas diferentes a la página.
+              
+          li 
+            i.fas.fa-layer-group(style="color: #039BE5;")
+            p.mb-0 Con código referenciado en otras páginas web ya publicadas en la red internet.
+              
     
+
+    .row.fondo3.mb-5.mb-lg-0
+      .col-lg-12.p-lg-5
+        .row.justify-content-center.mb-5
+          .col-11.col-lg-5.mb-4.mb-lg-0
+            .tarjeta.tarjeta-flip.color-secundario(@mouseover="indicadorTarjetaFlip = false")
+              .indicador--hover(v-if="indicadorTarjetaFlip")
+              .tarjeta-flip__contenedor
+                .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/tema3/img4.svg')})`}")
+                .tarjeta-flip__contenido.p-4.p-xl-5
+                  .code.pt-1.px-0
+                    code  &lt;html&gt; <br>&emsp;&lt;head&gt; <br>&emsp;&emsp;&lt;title&gt; Página web con código JavaScript embebido <br>&emsp;&emsp;&lt;/title&gt; <br>&emsp;&emsp; &lt;script type="text/javascript"&gt; insertar tag con atributo type indicando el uso del lenguaje JavaScript <br> alert(“Mi primer mensaje usando JavaScript”); <br>&emsp;&emsp;&emsp;&lt;/script&gt; <br>&emsp;&lt;/head&gt; <br>&emsp;&lt;body&gt; <br>&emsp;&emsp;&lt;p&gt; Contenido de la página web &lt;/p&gt;  <br>&emsp;&lt;/body&gt; <br>&lt;/html&gt;
+              
+                
+          .col-11.col-lg-5
+            .tarjeta.tarjeta-flip.color-secundario(@mouseover="indicadorTarjetaFlip = false")
+              .indicador--hover(v-if="indicadorTarjetaFlip")
+              .tarjeta-flip__contenedor
+                .tarjeta-flip__img(:style="{'background-image': `url(${require('@/assets/curso/tema3/img5.svg')})`}")
+                .tarjeta-flip__contenido.p-4.p-xl-5
+                  .code.pt-1.px-0
+                    code  &lt;html&gt; <br>&emsp;&lt;head&gt; <br>&emsp;&emsp;&lt;title&gt; Página web con código JavaScript embebido desde otros archivos fuera de la página actual. <br>&emsp;&emsp;&lt;/title&gt; <br>&emsp;&emsp; &lt;script src="static/js/miCodeUno.js"&gt; &lt;/script&gt; <br>&emsp;&emsp; &lt;script src="static/js/miCodeDos.js"&gt; &lt;/script&gt; <br>&emsp;&emsp;&lt;script src="static/js/miCodeTres.js"&gt; &lt;/script&gt; <br>&emsp;&lt;/head&gt; <br>&emsp;&lt;body&gt; <br>&emsp;&emsp;&lt;p&gt; Contenido de la página web &lt;/p&gt;  <br>&emsp;&lt;/body&gt; <br>&lt;/html&gt;
+                    
+        .row.justify-content-center.mb-0
+          .col-lg-10
+            p.mb-4 El resultado de la ejecución del código anterior se puede visualizar a continuación:
+            .row.justify-content-center
+              .col-4.col-lg-2
+                a.boton.color-secundario.indicador__container(@click="modal1 = true")(style="border-radius: 100px; background: linear-gradient(to bottom, #039BE5, #027FBC);")
+                  span.text-center 
+                    b.text-white Ejecutar en el navegador
+                  .indicador--click(v-if="mostrarIndicador")
+                    
+            ModalA(:abrir-modal.sync="modal1")
+              .row
+                .col-12
+                  figure.mb-4
+                    img.m-auto(src='@/assets/curso/tema3/img6.png' alt='Imagen ambiental' style='')
+
+
+
+    p.mb-4 Ejemplo de inclusión de código desde archivos con extensión .js en url diferentes a la de la página:
+
+          
+
+
     separador
     #t_3_2.titulo-segundo.color-acento-contenido
       .h2 3.2 Versiones
+
+    .row.justify-content-center
+      .col-10.col-lg-10
+        figure.mb-5
+          img.m-auto(src='@/assets/curso/tema3/img8.png' alt='Imagen ambiental' style='')
+
+    .row.justify-content-center.gx-0.mb-5
+      .col-lg-7.mb-4.mb-lg-0(style="background-color: #039BE5")
+        .p-5
+          p.mb-0.pt-lg-5.text-white El lenguaje JavaScript aparece aproximadamente a mediados de los años noventa elaborado por Brendan Eich, como una forma de mejorar la experiencia de usuario, puesto que en aquella época la velocidad de internet era demasiado lenta y se requería de un lenguaje del lado cliente al momento de realizar una petición web. Al año siguiente 1996 se propuso determinar un estándar, puesto que cada navegador lo interpretaba a su manera y por eso surgió ECMA-262 Ed.1 ES1. La evolución del lenguaje muestra en la siguiente línea del tiempo:
+      .col-10.col-lg-5
+        figure
+          img(src='@/assets/curso/tema3/img9.jpg', alt='imagen ambiental', style="").m-auto
+
+
+    figure
+      img(src='@/assets/curso/tema3/img10.svg', alt='linea de tiempo javascript', style="").m-auto
 
     separador
     #t_3_3.titulo-segundo.color-acento-contenido
@@ -39,6 +143,8 @@ export default {
   name: 'Tema3',
   data: () => ({
     // variables de vue
+    mostrarIndicador: true,
+    modal1: false,
   }),
   mounted() {
     this.$nextTick(() => {
